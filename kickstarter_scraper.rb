e8adc255abed1a2b7d0470b1207a543eb9636e2d
michael thomas <michael.thomas2014@gmail.com>
2020-05-25 14:01:26 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,6 +11,14 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   binding.pry
   
+  projects = {}
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+  
+  projects
+  
 end
 
 create_project_hash
